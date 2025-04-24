5792d6521220750b7e3dd1a2822892158b6cc9c6
Farid Kurbanou <kurbanou.faridun@gmail.com>
2025-04-24 16:06:18 +0300

    feat: carslist & card components add

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup>
 import Header from './components/Header.vue'
+import Cardlist from './components/Cardlist.vue'
 </script>
 
 <template>
-  <div class="w-4/5 m-auto bg-white h-screen rounded-xl shadow-xl mt-14">
+  <div class="w-4/5 m-auto bg-white rounded-xl shadow-xl mt-14">
     <Header />
+    <div class="p-10">
+      <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+      <Cardlist />
+    </div>
   </div>
 </template>
 
---
+++ src/components/Card.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div
+      class="relative bg-white border border-slate-100 rounded-3xl p-8 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl"
+    >
+      <img class="absolute top-8 left-8" src="/like-2.svg" alt="like 1" />
+      <img src="/sneakers/sneakers-1.jpg" alt="sneaker" />
+      <p class="mt-2">Мужские Кроссовки Nike Blazer Mid Suede</p>
+      <div class="flex justify-between">
+        <div class="flex flex-col mt-5">
+          <span class="text-slate-400"> Цена: </span>
+          <b> 1299 руб </b>
+        </div>
+        <img src="/plus.svg" alt="plus" />
+      </div>
+    </div>
+  </div>
+</template>
