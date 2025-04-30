6263ce3cec078192fe7a2ca30ccd1c9ab1dc7ad8
Farid Kurbanou <kurbanou.faridun@gmail.com>
2025-04-30 14:09:18 +0300

    feat:cart-item & cart-item-list add

---
+++ src/components/CartItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex items-center border border-slate-100 p-4 rounded-xl gap-4">
+    <img class="w-16" src="/sneakers/sneakers-1.jpg" alt="sneaker" />
+    <div class="flex flex-col">
+      <p>Кросовки Nike Air Max 270</p>
+      <div class="flex justify-between mt-2">
+        <b>12990 руб.</b>
+        <img
+          class="opacity-40 cursor-pointer transition hover:opacity-100"
+          src="/close.svg"
+          alt="close"
+        />
+      </div>
+    </div>
+  </div>
+</template>
