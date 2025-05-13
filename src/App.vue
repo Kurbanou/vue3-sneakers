16db52bf054f5f495a02234705ce33442e72cd19
Kurbanou <kurbanou.faridun@gmail.com>
2025-05-13 14:51:37 +0300

    feat: search & sort functions add

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,43 @@
 <script setup>
-import { onMounted, ref } from 'vue'
+import { onMounted, reactive, ref, watch } from 'vue'
 import axios from 'axios'
 import Header from './components/Header.vue'
 import CardList from './components/CardList.vue'
 import Drawer from './components/Drawer.vue'
 
 const items = ref([])
+const filters = reactive({
+  sortBy: '',
+  searchQuery: '',
+})
 
-onMounted(async () => {
+const onChangeSelect = (event) => {
+  filters.sortBy = event.target.value
+}
+const onChangeSearInput = (event) => {
+  filters.searchQuery = event.target.value
+}
+
+const fetchItems = async () => {
   try {
-    const { data } = await axios.get('https://604781a0efa572c1.mokky.dev/items')
+    const params = {
+      sortBy: filters.sortBy,
+    }
+
+    if (filters.searchQuery) {
+      params.title = `*${filters.searchQuery}*`
+    }
+    const { data } = await axios.get('https://604781a0efa572c1.mokky.dev/items', {
+      params,
+    })
+
     items.value = data
   } catch (error) {
     console.log(error)
   }
-})
+}
+onMounted(fetchItems)
+watch(filters, fetchItems)
 </script>
 
 <template>
@@ -25,24 +48,25 @@ onMounted(async () => {
       <div class="flex justify-between items-center">
         <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
         <div class="flex gap-4">
-          <select class="py-2 px-3 border rounded-md outline-none">
-            <option>По названию</option>
-            <option>По цене (дешевле)</option>
-            <option>По цене (дороже)</option>
+          <select @change="onChangeSelect" class="py-2 px-3 border rounded-md outline-none">
+            <option value="name">По названию</option>
+            <option value="price">По цене (дешевле)</option>
+            <option value="-price">По цене (дороже)</option>
           </select>
 
           <div class="relative">
             <img class="absolute left-4 top-3" src="/search.svg" alt="search" />
             <input
+              @input="onChangeSearInput"
               class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400"
               placeholder="Поиск..."
             />
           </div>
         </div>
       </div>
-    </div>
-    <div class="mt-10">
-      <CardList :items="items" />
+      <div class="mt-10">
+        <CardList :items="items" />
+      </div>
     </div>
   </div>
 </template>
